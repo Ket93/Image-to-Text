e08554affe8e2becc85757cb143c30e525790b38
Ket93 <kevincui113@gmail.com>
2021-09-18 13:01:15 -0400

    Added API connection

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const form = document.querySelector("#picURL");
+const text = document.querySelector("#returnText");
+const language = document.querySelector("#language");
 
 let myHeaders = new Headers();
 myHeaders.append("apikey", "FD00cuR4jklrGZnhXOHcn4GDFBlkhcJ9");
@@ -12,16 +14,24 @@ let requestOptions = {
 form.addEventListener("submit", async function (e) {
     e.preventDefault();
     const URL = form.elements.query.value;
-    const response = await axios.get(`https://api.promptapi.com/image_to_text/url?url=${URL}`, requestOptions);
-    makeText(response);
+    await fetch(`https://api.promptapi.com/image_to_text/url?url=${URL}`, requestOptions)
+        .then(response => response.json())
+        .then(textData => {
+            text.innerText = textData.all_text;
+            strLang = textData.lang.toUpperCase()
+            language.innerText = strLang;
+        })
+        .catch(error => console.log('error', error));
+
     form.elements.query.value = "";
 })
 
 const makeText = (response) => {
-    const language = document.createElement("p")
+    console.log(response);
+    const language = document.createElement("p");
     const returnText = document.createElement("p");
-    language.body = response.show.lang
-    returnText.body = response.show.all_text
+    language.body = response.lang
+    returnText.body = response.all_text
 
     document.body.append(language);
     document.body.append(returnText);
